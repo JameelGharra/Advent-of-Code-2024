8560178c16f2e935896a439f2e0d36ff0aaddf36
JameelGharra <jameelgharra@gmail.com>
2024-12-04 14:26:20 +0200

    Day 3 optimized - second part is now O(n)

--- Advent2024/Advent2024/Day3/Day3.cs
+++ Advent2024/Advent2024/Day3/Day3.cs
@@ -1,4 +1,5 @@
 ﻿using Advent2024.Common;
+using System;
 using System.Runtime.CompilerServices;
 public class Day3 : IDay {
 
@@ -40,51 +41,42 @@ public class Day3 : IDay {
         Console.WriteLine($"Part one - result: {result}");
     }
     private void SolvePartTwo() {
-        int index = 0;
+        bool allowed = true;
         long result = 0;
-        while (index < input.Length) {
-            int doIndex = input.IndexOf("do()", index);
-            int dontIndex = input.IndexOf("don't()", index);
-            int start = input.IndexOf("mul", index);
-
-            if (start == -1)
-                break;
-
-            if (dontIndex < start && (doIndex > start || doIndex == -1 || doIndex < dontIndex)) {
-                if (doIndex != -1) {
-                    index = doIndex + 4;
-                    continue;
-                }
-                break;
+        for (int i = 0; i < input.Length; ++i) {
+            if (input[i] == 'd' && i + 6 < input.Length && input.Substring(i, 7) == "don't()") {
+                allowed = false;
+                i += 6;
             }
-            // mul(...)dont()do()
-            // mul(...)do() mul() dont()
-            if (start + 3 < input.Length && input[start + 3] == '(') {
-                int innerStart = start + 3;
-                int innerEnd = input.IndexOf(')', innerStart);
-                if (innerEnd != -1 && innerEnd - innerStart - 1 <= 7 && innerEnd - innerStart - 1 >= 3) {
-                    string[] parts = input.Substring(innerStart + 1, innerEnd - innerStart - 1).Split(',');
-                    if (parts.Length == 2 &&
-                        parts[0].Length <= 3 &&
-                        parts[0].Length > 0 &&
-                        parts[1].Length > 0 &&
-                        parts[1].Length <= 3 &&
-                        int.TryParse(parts[0], out int num1) &&
-                        int.TryParse(parts[1], out int num2)) {
-                        result += num1 * num2;
-                        index = innerEnd + 1;
-                        continue;
+            else if (input[i] == 'd' && i + 3 < input.Length && input.Substring(i, 4) == "do()") {
+                allowed = true;
+                i += 3;
+            }
+            else if (allowed && input[i] == 'm' && i + 2 < input.Length && input.Substring(i, 3) == "mul") {
+                if (i + 3 < input.Length && input[i + 3] == '(') {
+                    int innerStart = i + 3;
+                    int innerEnd = input.IndexOf(')', innerStart);
+                    if (innerEnd != -1 && innerEnd - innerStart - 1 <= 7 && innerEnd - innerStart - 1 >= 3) {
+                        string[] parts = input.Substring(innerStart + 1, innerEnd - innerStart - 1).Split(',');
+                        if (parts.Length == 2 &&
+                            parts[0].Length <= 3 &&
+                            parts[0].Length > 0 &&
+                            parts[1].Length > 0 &&
+                            parts[1].Length <= 3 &&
+                            int.TryParse(parts[0], out int num1) &&
+                            int.TryParse(parts[1], out int num2)) {
+                            result += num1 * num2;
+                            i = innerEnd;
+                        }
                     }
                 }
-
             }
-            index += 3;
         }
         Console.WriteLine($"Part two - result: {result}");
     }
     public void Solve() {
         SolvePartOne(); // 187825547
-        SolvePartTwo();
+        SolvePartTwo(); // 85508223
     }
 }
 
